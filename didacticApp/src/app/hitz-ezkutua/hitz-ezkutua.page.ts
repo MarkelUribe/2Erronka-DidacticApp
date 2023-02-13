0f81fcd30ce0413f6f4f114406c839ae810a3e14
MarkelUribe <75306077+MarkelUribe@users.noreply.github.com>
2023-02-13 09:59:30 +0100

    hitz ezkutua

--- didacticApp/src/app/hitz-ezkutua/hitz-ezkutua.page.ts
+++ didacticApp/src/app/hitz-ezkutua/hitz-ezkutua.page.ts
@@ -9,41 +9,32 @@ let imageArray = [];
   styleUrls: ['./hitz-ezkutua.page.scss'],
 })
 export class HitzEzkutuaPage {
-  clickedImageId: string;
-  hitzEzkutua: string;
+  message: string;
+
   kontagailua = 0;
-  constructor(private router: Router) {}
+  constructor() {}
 
-  handleClick(event: any) {
-    const imageId = event.target.id;
-    //Anbotoko Mariren kondaira
-    imageArray.push(imageId);
-    console.log(imageArray);
-
-    const imageString = imageArray.join('');
-    console.log(imageString); // 'anboto'
-    this.clickedImageId = imageString;
-
-    if (imageArray.length == 25 && imageString == 'anbotoko mariren kondaira') {
-      this.hitzEzkutua = imageString;
-      this.clickedImageId = 'Zorionak!!! Hitz ezkutua aurkitu duzu!!!';
-      localStorage.setItem('fase', '6');
-      this.router.navigate(['/home']);
-      imageArray = [];
-    } else if (
-      imageArray.length == 25 &&
-      imageString != 'anbotoko mariren kondaira'
+  submitMessage() {
+    console.log(this.message);
+    if (
+      this.message == 'Anbotoko Mariren Kondaira' ||
+      this.message == 'anbotoko mariren kondaira'
     ) {
-      this.clickedImageId = '';
+      // Lehio bat sartu, erabiltzailea zorionduz!!!
+      console.log('Oso ondo!');
+    } if (this.message == '') {
+      // Lehio bat sartu, ez duela ezer idatzi azalduz!!!
+      console.log('Ez duzu ezer idatzi!!!');
+    } else {
+      // Erabiltzaileak 3 aukera duenez, geratzen zaion aukerak agertzea lehio baten laguntzaz.
+      this.message = '';
       this.kontagailua++;
-      if (this.kontagailua == 1) {
-        this.clickedImageId = '2 aukera geratzen zaizu!!!';
-      } else if (this.kontagailua == 2) {
-        this.clickedImageId = 'Aukera 1 geratzen zaizu!!!';
-      } else if (this.kontagailua == 3) {
-        this.clickedImageId = 'Barkatu, baina aukera guztiak bukatu zaizu!!!';
+      console.log(this.kontagailua);
+
+      if (this.kontagailua == 3) {
+        // Lehio bat irekitzea, aukerak bukatu direla azalduz eta mapara bueltatu.
+        console.log('Aukerak bukatu dira!!!');
       }
-      imageArray = [];
     }
   }
 }
