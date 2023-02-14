659250567d794a2ab413e60f1f6c244dbf3ee15a
Oihana1 <oaranba.azkoitia@gmail.com>
2023-02-14 09:41:04 +0100

    capacitor sync android

--- didacticApp/capacitor.config.ts
+++ didacticApp/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.didaktikapp.app',
+  appId: 'io.ionic.starter',
   appName: 'didacticApp',
   webDir: 'www',
   bundledWebRuntime: false,
